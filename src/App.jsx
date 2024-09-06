4afb5aedb70358488925895810e640e0f015f3ec
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-06 16:20:51 +0530

    add plus button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(){
+    super()
+
+    this.state({
+      count: 0
+    })
+  }
   render() {
     return (
-      <div>App</div>
+      <>
+      
+      <button onClick={()=> this.setState({count: this.state.count + 1})}>Plus</button>
+      <h1>{this.state.count}</h1>
+      <button onClick={()=> this.state}>Minus</button>
+      
+      </>
     )
   }
 }
