d766c4bf130173475650ffb501b2ed17ac2d6190
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-06 16:24:19 +0530

    clean up index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(){
-    super()
-
-    this.state({
-      count: 0
-    })
+  constructor() {
+    super();
+    this.state = ({ count: 0 });
   }
   render() {
     return (
       <>
-      
-      <button onClick={()=> this.setState({count: this.state.count + 1})}>Plus</button>
-      
-      <h1>{this.state.count}</h1>
-      <button onClick={()=> this.setState({count: this.state.count - 1})}>Minus</button>
-      
-      
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Plus
+        </button>
+
+        <h1>{this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          Minus
+        </button>
       </>
-    )
+    );
   }
 }
